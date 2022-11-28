87d8849ab8cd6e53a9755ea25a975380e03f7f85
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-11-28 13:12:43 +0530

    Add files via upload

---
+++ Coding Questions/C3/script.js
@@ -0,0 +1,10 @@
+/*There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculatetheaveragescoreforeachteam,usingthetestdatabelow
+2. Comparetheteam'saveragescorestodeterminethewinnerofthecompetition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus1:Includearequirementforaminimumscoreof100.Withthisrule,a
+team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. Bonus2:Minimumscorealsoappliestoadraw!Soadrawonlyhappenswhen both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy*/
+
