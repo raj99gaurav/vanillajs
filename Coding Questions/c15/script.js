a99b62112c520ab1d60de3c0226aab075aa1eb03
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-12-18 13:17:21 +0530

    Add files via upload

---
+++ Coding Questions/c15/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*This is more of a thinking challenge than a coding challenge 🤓 Your tasks:
+1. TaketheIIFEbelowandattheendofthefunction,attachaneventlistenerthat changes the color of the selected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again!
+2. Andnowexplaintoyourself(orsomeonearoundyou)whythisworked!Takeall the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example.
+ 
+(function () {
+const header = document.querySelector('h1'); header.style.color = 'red';
+})();
+GOOD LUCK 😀 */
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
